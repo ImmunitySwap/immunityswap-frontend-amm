471a1ad94a0ee9713cc85b50bc8e6cbc36a283f2
Maximiliaan van Dijk <maximiliaan@allianceblock.io>
2021-03-19 14:26:05 -0600

    updated priceticker

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0x29927a1Ad6EE13357D276bA3A26bFc90371DE141',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x9b989a7b8963f4b08ec094710e2966fb3c7f6c43'
+  lpAddress: '0x46Efe550E21D77300470eDeE45e2EB9566be6f65'
 }
 
 export default priceContracts
